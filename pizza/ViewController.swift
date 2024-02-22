38ccb18ffc306786755cab2c81d86a3a55d5d2bb
Lee espiritu <espiritu.lee@gmail.com>
2024-02-22 17:22:49 +1100

    Home Screen

--- pizza/ViewController.swift
+++ pizza/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button_login: UIButton!
+    @IBOutlet weak var button_signUp: UIButton!
     //Test
     override func viewDidLoad() {
         super.viewDidLoad()
