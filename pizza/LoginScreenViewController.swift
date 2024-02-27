e0f6866a567751dd8ed8d07a842d9c63e503ff75
Lee Espiritu <espiritu.lee@gmail.com>
2024-02-26 21:37:47 -0800

    Added login screen view controller prototype layout and class

---
+++ pizza/LoginScreenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginScreenViewController.swift
+//  pizza
+//
+//  Created by user936262 on 2/26/24.
+//
+
+import UIKit
+
+class LoginScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
